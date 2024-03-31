32c1c0fcd100220326effd4215704daa80d3f2de
fretgfr <51489753+fretgfr@users.noreply.github.com>
2024-03-30 23:44:54 -0400

    Initial schema.

---
+++ schema.sql
@@ -0,0 +1,74 @@
+
+-- in current implementation this holds prefix disable status.
+-- not sure if this will be needed at all.
+CREATE TABLE IF NOT EXISTS guilds (
+    guild_id                BIGINT PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id                 BIGINT PRIMARY KEY,
+    opted_out               BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS delete_snipe (
+    id                      BIGSERIAL PRIMARY KEY,
+    user_id                 BIGINT NOT NULL,
+    channel_id              BIGINT NOT NULL,
+    guild_id                BIGINT NOT NULL,
+    content                 VARCHAR(4096) NOT NULL DEFAULT '',
+    timestamp               TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    referenced_message_id   BIGINT DEFAULT NULL
+);
+
+-- maybe disambiguate and call it deleted_message_attachments
+CREATE TABLE IF NOT EXISTS message_attachments (
+    id                      BIGSERIAL PRIMARY KEY,
+    delete_snipe_id         BIGINT REFERENCES delete_snipe(id) ON DELETE CASCADE,
+    filename                TEXT NOT NULL,  -- TODO this should probably be a varchar with the discord filename length limit.
+    proxy_url               TEXT            -- prolly here too
+);
+
+-- same here, maybe deleted_message_stickers
+CREATE TABLE IF NOT EXISTS message_stickers (
+    id                      BIGSERIAL PRIMARY KEY,
+    delete_snipe_id         BIGINT REFERENCES delete_snipe(id) ON DELETE CASCADE,
+    name                    TEXT, -- TODO maybe not needed?
+    url                     TEXT  -- TODO varchar may be more appropriate, idk.
+);
+
+CREATE TABLE IF NOT EXISTS edit_snipe (
+    id                      BIGSERIAL PRIMARY KEY,
+    user_id                 BIGINT NOT NULL,
+    message_id              BIGINT NOT NULL,
+    channel_id              BIGINT NOT NULL,
+    guild_id                BIGINT NOT NULL,
+    before_content          VARCHAR(4096) NOT NULL DEFAULT '',
+    after_content           VARCHAR(4096) NOT NULL DEFAULT '',
+    timestamp               TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS edited_message_attachment (
+    id                      BIGSERIAL PRIMARY KEY,
+    delete_snipe_id         BIGINT REFERENCES delete_snipe(id) ON DELETE CASCADE,
+    filename                TEXT NOT NULL,  -- TODO this should probably be a varchar with the discord filename length limit.
+    proxy_url               TEXT            -- prolly here too
+);
+
+CREATE TABLE IF NOT EXISTS reaction_snipe (
+    id                      BIGSERIAL PRIMARY KEY,
+    user_id                 BIGINT NOT NULL,
+    message_id              BIGINT NOT NULL,
+    channel_id              BIGINT NOT NULL,
+    guild_id                BIGINT NOT NULL,
+    is_custom_emoji         BOOLEAN NOT NULL,
+    custom_emoji_name       VARCHAR(xxx) DEFAULT NULL,
+    emoji                   TEXT NOT NULL, -- either the name of the emoji, or the url to a custom emoji.
+    timestamp               TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS error_log (
+    id                      BIGSERIAL PRIMARY KEY,
+    item                    TEXT NOT NULL DEFAULT '',
+    traceback               TEXT NOT NULL,
+    timestamp               TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
